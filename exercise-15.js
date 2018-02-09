6367107b40cc9e97095277c5199725705767b0d6
asepridwan1989 <aseprmail@gmail.com>
2018-02-09 15:23:12 +0700

    upload

--- exercise-15.js
+++ exercise-15.js
@@ -23,6 +23,6 @@ function groupAnimals(animals) {
 
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-// [ ['ayam', 'anoa'], ['cacing'], ['kuda'] ]
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
